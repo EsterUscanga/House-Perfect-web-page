2d3864a22494b4ea830fb920cda01e4a4f134bd3
Ester Uscanga <estereuo@gmail.com>
2018-04-08 08:07:38 -0500

    Gallery option

--- app.js
+++ app.js
@@ -35,75 +35,97 @@ app.use(express.static(path.join(__dirname, pathClient))) // para crear el clien
 var db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password:'',
+  password: '',
   database: 'inmobiliaria'
 })
 
 // Connect
-db.connect(function(err){
-  if(err){
+db.connect(function (err) {
+  if (err) {
     throw err;
   }
   console.log('MySQL Connected')
 })
 
-  /*----------------------------------*/
- /*  Queries to filter information   */
-/*----------------------------------*/
 
-
-const atributosBienraiz = ['casas', 'departamentos', 'bodegas', 'oficinas', 'terrenos']
-const tiposBienraiz = ['casa', 'departamento', 'bodega', 'oficina', 'terreno']
-
-let queryBienRaiz = []
-
-for (let i in tiposBienraiz){
-  queryBienRaiz[i] = `
-  SELECT id_${tiposBienraiz[i]}
-  FROM ${atributosBienraiz[i]}
-  `
-}
-
-let resultsTipobienraiz = []
-
-
-app.get('/', function(req, res){  
+app.get('/index', function (req, res) {
   res.render('index')
 })
 
-app.get('/quienes', function(req, res){  
+app.get('/quienes', function (req, res) {
   res.render('quienes')
 })
 
-app.get('/servicios', function(req, res){  
+app.get('/servicios', function (req, res) {
   res.render('servicios')
 })
 
-app.get('/busqueda', function(req, res){  
+app.get('/busqueda', function (req, res) {
   res.render('busqueda')
 })
 
-app.get('/contacto', function(req, res){  
+app.get('/contacto', function (req, res) {
   res.render('contacto')
 })
 
-app.post('/busqueda', function(req, res){
-  const tipoBien = req.body.combo
-  db.query(tipoBien, function(err, result) {
+app.post('/busqueda', function (req, res) {
+  const bien = req.body.combo
+  let tipoBien = []
+
+  switch (bien) {
+    case 'div1': tipoBien.push('oficina')
+      break
+
+    case 'div2': tipoBien.push('casa')
+      break
+
+    case 'div3': tipoBien.push('departamento')
+      break
+
+    case 'div4': tipoBien.push('bodega')
+      break
+
+    case 'div5': tipoBien.push('terreno')
+      break
+  }
+
+  const status = req.body.Status
+  tipoBien.push(status)
+
+  console.log(tipoBien)
+
+    /*----------------------------------*/
+   /*  Queries to filter information   */
+  /*----------------------------------*/
+
+  // CASAS
+
+  queryBienRaiz = `
+  SELECT DISTINCT
+    ${tipoBien[0]}s.id_${tipoBien[0]}
+  FROM 
+    ${tipoBien[0]}s,
+    bienes_raices
+  WHERE bienes_raices.status = '${tipoBien[1]}'
+  `
+
+  db.query(queryBienRaiz, function (err, result) {
     if (err)
       throw err
-    else{
+    else {
       console.log(result)
-    }  
+    }
   })
 })
 
-
+app.get('/galeria', function (req, res) {
+  res.render('galeria')
+})
 /*
 app.get('/busqueda.ejs', function (req, res) {
   res.render('busqueda')
 })
 */
-app.listen(port, function(){
+app.listen(port, function () {
   console.log(messagePort)
 })
