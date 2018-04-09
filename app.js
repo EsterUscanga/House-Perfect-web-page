0982342478fcde63e8c94ccc1eb4987b3b02557a
Ester Uscanga <estereuo@gmail.com>
2018-04-08 21:50:05 -0500

    Altas oficinas

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mysql = require('mysql')
 const bodyParser = require('body-parser')
 const path = require('path')
+const parseInt = require('parse-int')
 
 const port = '8380'
 const messagePort = 'Server started on  http://localhost:' + port + '/index'
@@ -94,8 +95,8 @@ app.post('/busqueda', function (req, res) {
 
   console.log(tipoBien)
 
-    /*----------------------------------*/
-   /*  Queries to filter information   */
+  /*----------------------------------*/
+  /*  Queries to filter information   */
   /*----------------------------------*/
 
   queryBienRaiz = `
@@ -114,7 +115,7 @@ app.post('/busqueda', function (req, res) {
   })
 })
 
-app.post('/contacto', function(req, res){
+app.post('/contacto', function (req, res) {
   const nombre = req.body.Nombre
   const aPaterno = req.body.paterno
   const aMaterno = req.body.materno
@@ -125,12 +126,12 @@ app.post('/contacto', function(req, res){
   INSERT INTO cliente (nombre_cliente, apellido_pat_cliente, apellido_mat_cliente, telefono, correo) VALUES
   ('${nombre}', '${aPaterno}', '${aMaterno}', '${telefono}', '${correo}');
   `
- 
+
   db.query(queryInsersion, function (err, result) {
     if (err)
       throw err
     else {
-      console.log('One record inserted')
+      console.log('One record inserted(en clientes)')
     }
   })
 
@@ -139,6 +140,138 @@ app.post('/contacto', function(req, res){
 app.get('/galeria', function (req, res) {
   res.render('galeria')
 })
+
+app.get('/login', function (req, res) {
+  res.render('login')
+})
+
+app.post('/login', function (req, res) {
+  const username = req.body.Username
+  const password = req.body.Password
+
+  queryValidacion = `
+  SELECT 
+    nombre_usuario,
+    contrasena
+  FROM usuarios
+  `
+  db.query(queryValidacion, function (err, result) {
+    if (err)
+      throw err
+    else {
+      if (username == result[0]['nombre_usuario'] && password == result[0].contrasena) {
+        res.render('opcionABC')
+      }
+    }
+  })
+
+})
+
+app.post('/opcionABC', function (req, res) {
+  const ABC = req.body.select
+  console.log(ABC)
+  res.render(ABC)
+
+})
+
+app.post('/altas', function (req, res) {
+
+  let array = []
+
+  const bien = req.body.select
+  array.push(bien)
+  const ubicacion = req.body.Ubicacion
+  array.push(ubicacion)
+  const precio = req.body.Precio
+  array.push(precio)
+  const superficie = req.body.Superficie
+  array.push(superficie)
+  const construccion = req.body.Construccion
+  array.push(construccion)
+  const caracteristicas = req.body.Caracteristicas
+  array.push(caracteristicas)
+  const status = req.body.Status
+  array.push(status)
+
+
+  switch (bien) {
+    case 'div1':
+    const noBanos = req.body.bano
+    array.push(noBanos)
+    const recepcion = req.body.Recepcion
+    array.push(recepcion)
+    const estacionamiento = req.body.Estacionamiento
+    array.push(estacionamiento)
+
+    console.log(array)
+
+   
+    const query = `
+      SELECT id_oficina FROM oficinas
+      WHERE id_oficina =(SELECT MAX(id_oficina) FROM oficinas)
+    `
+    db.query(query, function (err, result) {
+      if (err)
+        throw err
+      else {
+  
+        console.log(result)
+
+        let id = parseInt(result[0]['id_oficina']) + 1
+
+        console.log( )
+        
+        
+        const queryOficinas_bienRaiz = `
+        INSERT INTO bienes_raices (ubicacion, precio, superficie, contruccion, caracteristicas, status, cod_departamento, cod_bodega, cod_casa, cod_terreno, cod_oficina, cod_compra) VALUES
+        ('${ubicacion}', '${precio}', '${superficie}', '${construccion}', '${caracteristicas}', '${status}', ${parseInt(result[0]['id_oficina']) + 1}, 1, 1, 1, 1, 1);
+        `
+        console.log(queryOficinas_bienRaiz)
+      
+        const queryOficinas = `
+        INSERT INTO oficinas (num_bano, recepcion, estacionamiento) VALUES
+        ('${noBanos}', '${recepcion}', '${estacionamiento}');
+        `
+        db.query(queryOficinas_bienRaiz, function (err, result) {
+          if (err)
+            throw err
+          else {
+            console.log('One record inserted(En tabla bienes_raices)')
+          }
+        })
+  
+        db.query(queryOficinas, function (err, result) {
+          if (err)
+            throw err
+          else {
+            console.log('One record inserted(En tabla bienes_raices)')
+          }
+        })
+        
+      }
+    })
+      break
+
+    case 'div2':
+
+      break
+
+    case 'div3':
+
+      break
+
+    case 'div4':
+
+      break
+
+    case 'div5':
+
+      break
+  }
+
+
+})
+
 
 app.listen(port, function () {
   console.log(messagePort)
